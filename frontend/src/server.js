c207217aed400fbc871466d684d1974d3f0c7c24
dev-amber <ambernaz1000@gmail.com>
2026-02-23 15:52:35 +0500

    change backend_url done

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,6 +1,4 @@
-//export const server = "http://localhost:8000/api/v2";
 
-//export const backend_url= "http://localhost:8000/"
 
 export const server = "https://multivendor-e-shop.vercel.app/api/v2";
 
