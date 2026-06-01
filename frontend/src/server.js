fa0ebb698d85fe5a631887df145a7b63630706dd
dev-amber <ambernaz1000@gmail.com>
2026-06-01 16:45:47 +0500

    change url

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,6 +1,6 @@
 
 export const server = "https://multivendor-e-shop-six.vercel.app/api/v2";
 
-export const backend_url="https://multivendor-e-shop-six.vercel.app"
+export const backend_url="https://multivendor-e-shop-six.vercel.app/"
 
 
