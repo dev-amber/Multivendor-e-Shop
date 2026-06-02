02076f5fd1e72000245575e1a1e218180da76eea
dev-amber <ambernaz1000@gmail.com>
2026-06-02 14:23:08 +0500

    styling

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -32,9 +32,9 @@ const Login = () => {
       });
   };
   return (
-    <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+    <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8 to-[#8292AB]">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
-        <h2 className="mt-6 text-3xl font-extrabold text-gray-900 text-center">
+        <h2 className="mt-6 text-3xl font-extrabold text-gray-900 text-center mt-5">
           Login to your account
         </h2>
       </div>
