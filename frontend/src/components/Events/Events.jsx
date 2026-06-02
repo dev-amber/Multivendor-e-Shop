6956a88aa47bf17287624ad21ab7e0960dfa1ccf
dev-amber <ambernaz1000@gmail.com>
2026-06-02 17:25:52 +0500

    evet

--- frontend/src/components/Events/Events.jsx
+++ frontend/src/components/Events/Events.jsx
@@ -1,40 +1,32 @@
-import React from 'react'
-import { useSelector } from 'react-redux';
-import styles from '../../styles/style'
+import React from "react";
+import { useSelector } from "react-redux";
+import styles from "../../styles/style";
 import EventCard from "./EventCard";
 
 const Events = () => {
-  const {allEvents,isLoading} = useSelector((state) => state.event);  
-   
+  const { allEvents = [], isLoading } = useSelector(
+    (state) => state.event || {}
+  );
+
   return (
     <div>
-     {
-      !isLoading && (
+      {!isLoading && (
         <div className={`${styles.section}`}>
-      <div className={`${styles.heading}`}>
-        <h1>Popular Events</h1>
-      </div>
+          <div className={`${styles.heading}`}>
+            <h1>Popular Events</h1>
+          </div>
 
-      <div className="w-full grid">
-         {
-          allEvents.length !== 0 && (
-            <EventCard data={allEvents && allEvents[0]} />
-          )
-         }
-         <h4>{
-           allEvents?.length === 0 && (
-            'No Events have!'
-           )
-          }
-
-         </h4>
-      </div>
-     
+          <div className="w-full grid">
+            {allEvents?.length > 0 ? (
+              <EventCard data={allEvents[0]} />
+            ) : (
+              <h4>No Events have!</h4>
+            )}
+          </div>
+        </div>
+      )}
     </div>
-      )
-     }
-  </div>
-  )
-}
+  );
+};
 
-export default Events
+export default Events;
